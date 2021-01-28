ede27f651bed91d1d609f53ece285a8ef9722851
Seojunhwan <seojunhwan@kakao.com>
2021-01-29 01:33:43 +0900

    create HTML & CSS & JS
    add clock, greeting, todo form

---
+++ index.js
@@ -0,0 +1,22 @@
+const title = document.querySelector("#title"),
+  body = document.querySelector("body"),
+  clock = document.querySelector(".js-title");
+
+const CLICKED_CLASS = "clicked",
+  CHANGE_CLASS = "first_color";
+
+function handleClick() {
+  title.classList.toggle(CLICKED_CLASS);
+}
+
+function changeColor() {
+  title.classList.toggle(CHANGE_CLASS);
+  clock.classList.toggle(CHANGE_CLASS);
+}
+
+function init() {
+  title.addEventListener("click", handleClick);
+  setInterval(changeColor, 2000);
+}
+
+init();
